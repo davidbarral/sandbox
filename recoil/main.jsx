61b757e47280658afccf31c4d5c337cf43b6066c
David Barral <david@trabe.io>
2022-07-21 14:36:50 +0200

    Move recoil to its own folder

--- recoil/main.jsx
+++ recoil/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Recoil from "./boxes/recoil.jsx";
+import Recoil from "./recoil.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<Recoil />);
