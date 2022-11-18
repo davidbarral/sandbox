92d556028b2be358f1dea6664acee6d2dc1b9daa
David Barral <david@trabe.io>
2022-11-18 11:22:48 +0100

    Extract functions

--- refactoring/notifier.js
+++ refactoring/notifier.js
@@ -1,24 +1,38 @@
 const { postMessage } = require("./trello.js");
 
-const notifyUpload = ({ bucket, key }) => {
-  if (bucket === "people") {
-    if (/^\/nominas\//.test(key)) {
-      const [_, login, year, month] = key.match(/^\/nominas\/(\S+?)\/(\d{4})\/(\w+)\.pdf$/);
-      postMessage({
-        channels: ["david.barral"],
-        message: `Nueva nómina de ${month} ${year}`,
-      });
-    } else if (/^\/documents\//.test(key)) {
-      const [_, login, doc] = key.match(/^\/documents\/(\S+?)\/(\S+)$/);
-      postMessage({ channels: [login], message: `Nuevo documento ${doc}` });
-    } else {
-      throw new Error(`Cannot process people${key}`);
-    }
-  } else if (bucket === "projects") {
-    const [_, project, doc] = key.match(/^\/(\S+?)\/(\S+)$/);
-    postMessage({ channels: [project], message: `Nuevo documento: ${doc}` });
+const isPersonSalary = ({ bucket, key }) => bucket === "people" && /^\/nominas\//.test(key);
+
+const isPersonDocument = ({ bucket, key }) => bucket === "people" && /^\/documents\//.test(key);
+
+const isProjectDocument = ({ bucket }) => bucket === "projects";
+
+const nofityPersonSalary = ({ bucket, key }) => {
+  const [_, login, year, month] = key.match(/^\/nominas\/(\S+?)\/(\d{4})\/(\w+)\.pdf$/);
+  postMessage({
+    channels: ["david.barral"],
+    message: `Nueva nómina de ${month} ${year}`,
+  });
+};
+
+const notifyPersonDocument = ({ bucket, key }) => {
+  const [_, login, doc] = key.match(/^\/documents\/(\S+?)\/(\S+)$/);
+  postMessage({ channels: [login], message: `Nuevo documento ${doc}` });
+};
+
+const notifyProjectDocument = ({ bucket, key }) => {
+  const [_, project, doc] = key.match(/^\/(\S+?)\/(\S+)$/);
+  postMessage({ channels: [project], message: `Nuevo documento: ${doc}` });
+};
+
+const notifyUpload = (upload) => {
+  if (isPersonSalary(upload)) {
+    nofityPersonSalary(upload);
+  } else if (isPersonDocument(upload)) {
+    notifyPersonDocument(upload);
+  } else if (isProjectDocument(upload)) {
+    notifyProjectDocument(upload);
   } else {
-    throw new Error(`Cannot process ${bucket}`);
+    throw new Error(`Cannot process ${upload.bucket}/${upload.key}`);
   }
 };
 
