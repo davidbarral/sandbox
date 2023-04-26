6875e8fc9519716c7c8814db17a468069593b969
David Barral <david@trabe.io>
2023-04-26 13:11:39 +0200

    Typed but not useful

--- ts-advanced-types/events.ts
+++ ts-advanced-types/events.ts
@@ -1,14 +1,16 @@
-const eventBus = () => {
-  const handlers = {};
+type Callback = (...args: any[]) => void;
 
-  const on = (event, cb) => {
+const eventBus = () => {
+  const handlers: Record<string, Callback[]> = {};
+
+  const on = (event: string, cb: Callback) => {
     const cbs = handlers[event] ?? [];
     cbs.push(cb);
     handlers[event] = cbs;
   };
 
-  const emit = (event, ...args) => {
-    handlers[event]?.forEach((cb) => {
+  const emit = (event: string, ...args: any[]) => {
+    handlers[event]?.forEach((cb: Callback) => {
       cb(...args);
     });
   };
