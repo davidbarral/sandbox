389254b63944f77a13fec7a42b3e3540dfe5b2f9
David Barral <david@trabe.io>
2024-07-15 13:34:50 +0200

    Added ChatGPT integration example

---
+++ chatgpt/index.js
@@ -0,0 +1,54 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+   project: process.env.OPENAI_PROJECT,
+ });
+
+const sendMessage = async (...prompt) => {
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: prompt,
+      model: "gpt-4-turbo",
+      temperature: 0.5,
+      max_tokens: 1000,
+    });
+
+    return {
+      ok: true,
+      content: completion.choices[0].message.content,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      error,
+    };
+  }
+};
+
+const prose = await sendMessage(
+ { role: "system", content: "Eres un gran escritor de cuentos" },
+ { role: "user",  content: "Cuéntame un cuento sobre Miriam, la joven de Boiro que se fue a la gran ciudad de Coruña para ser informática y acabo trabajando en Trabe Soluciones, la mejor empresa del mundo en 200 palabras" },
+);
+
+console.log(prose.content, "\n\n");
+
+const verses = await sendMessage(
+{ role: "system", content: "Eres un gran escritor de cuentos" },
+{ role: "user",  content: "Cuéntame un cuento en 200 palabras" },
+{ role: "assistant", content: prose.content },
+{ role: "user",  content: "Ahora en verso por favor" },
+);
+
+console.log(verses.content, "\n\n");
+
+// const haiku = await sendMessage(
+// { role: "system", content: "Eres un gran escritor de cuentos" },
+// { role: "user",  content: "Cuéntame un cuento en 200 palabras" },
+// { role: "assistant", content: prose.content },
+// { role: "user",  content: "Ahora en verso por favor" },
+// { role: "assistant", content: verses.content },
+// { role: "user",  content: "Mejor como un haiku" },
+// );
+
+// console.log(haiku.content);
