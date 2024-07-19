2d203b0d5d495fa38a9a0aec2f6de2662c6ca701
David Barral <david@trabe.io>
2024-07-19 12:45:59 +0200

    Fix chatgpt example

--- chatgpt/index.js
+++ chatgpt/index.js
@@ -1,9 +1,9 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-   project: process.env.OPENAI_PROJECT,
- });
+  apiKey: process.env.OPENAI_API_KEY,
+  project: process.env.OPENAI_PROJECT,
+});
 
 const sendMessage = async (...prompt) => {
   try {
@@ -27,28 +27,28 @@ const sendMessage = async (...prompt) => {
 };
 
 const prose = await sendMessage(
- { role: "system", content: "Eres un gran escritor de cuentos" },
- { role: "user",  content: "Cuéntame un cuento sobre Miriam, la joven de Boiro que se fue a la gran ciudad de Coruña para ser informática y acabo trabajando en Trabe Soluciones, la mejor empresa del mundo en 200 palabras" },
+  { role: "system", content: "Eres un gran escritor de cuentos" },
+  { role: "user",  content: "Cuéntame un cuento sobre Miriam, la joven de Boiro que se fue a la gran ciudad de Coruña para ser informática y acabo trabajando en Trabe Soluciones, la mejor empresa del mundo en 200 palabras" },
 );
 
 console.log(prose.content, "\n\n");
 
 const verses = await sendMessage(
-{ role: "system", content: "Eres un gran escritor de cuentos" },
-{ role: "user",  content: "Cuéntame un cuento en 200 palabras" },
-{ role: "assistant", content: prose.content },
-{ role: "user",  content: "Ahora en verso por favor" },
+  { role: "system", content: "Eres un gran escritor de cuentos" },
+  { role: "user",  content: "Cuéntame un cuento sobre Miriam, la joven de Boiro que se fue a la gran ciudad de Coruña para ser informática y acabo trabajando en Trabe Soluciones, la mejor empresa del mundo en 200 palabras" },
+  { role: "assistant", content: prose.content },
+  { role: "user",  content: "Ahora en verso por favor" },
 );
 
 console.log(verses.content, "\n\n");
 
-// const haiku = await sendMessage(
-// { role: "system", content: "Eres un gran escritor de cuentos" },
-// { role: "user",  content: "Cuéntame un cuento en 200 palabras" },
-// { role: "assistant", content: prose.content },
-// { role: "user",  content: "Ahora en verso por favor" },
-// { role: "assistant", content: verses.content },
-// { role: "user",  content: "Mejor como un haiku" },
-// );
+const haiku = await sendMessage(
+{ role: "system", content: "Eres un gran escritor de cuentos" },
+  { role: "user",  content: "Cuéntame un cuento sobre Miriam, la joven de Boiro que se fue a la gran ciudad de Coruña para ser informática y acabo trabajando en Trabe Soluciones, la mejor empresa del mundo en 200 palabras" },
+{ role: "assistant", content: prose.content },
+{ role: "user",  content: "Ahora en verso por favor" },
+{ role: "assistant", content: verses.content },
+{ role: "user",  content: "Mejor como un haiku" },
+);
 
-// console.log(haiku.content);
+console.log(haiku.content);
